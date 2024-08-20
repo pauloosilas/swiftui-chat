5dbf92b8a78d45f8de498effba4cec81aeaa7940
pauloosilas <paulo3356@gmail.com>
2024-08-20 12:06:30 -0300

    add layout login screen

---
+++ SwiftUIChatFirestore/View/Authentication/RegistrationView.swift
@@ -0,0 +1,14 @@
+//
+//  RegistrationView.swift
+//  SwiftUIChatFirestore
+//
+//  Created by Paulo on 20/08/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View{
+    var body: some View{
+        Text("")
+    }
+}
