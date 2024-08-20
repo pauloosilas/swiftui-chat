f1ce35ab5b6237d1112bc46a698d862c29230405
pauloosilas <paulo3356@gmail.com>
2024-08-20 10:37:12 -0300

    Initial Commit

---
+++ SwiftUIChatFirestore/SwiftUIChatFirestoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatFirestoreApp.swift
+//  SwiftUIChatFirestore
+//
+//  Created by Paulo on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatFirestoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
